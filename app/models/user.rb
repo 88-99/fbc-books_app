4121d602a9b4d228ecd43083d42c98f5410bfbe9
88-99 <xz835.525o39@yahoo.com>
2023-01-27 03:41:16 +0900

    関連付けを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :active_follows, foreign_key: 'follower_id', class_name: 'Follow', dependent: :destroy
+  has_many :passive_follows, foreign_key: 'followed_id', class_name: 'Follow', dependent: :destroy
+  has_many :following, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
   has_one_attached :avatar
 end
