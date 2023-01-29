b1768d384c7939a22619465c164d31e3c59020e7
88-99 <xz835.525o39@yahoo.com>
2023-01-29 21:42:20 +0900

    RuboCop 修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class FollowsController < ApplicationController
   def create
     following = current_user.follow(params[:format])
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :active_follows, foreign_key: 'follower_id', class_name: 'Follow', dependent: :destroy
-  has_many :passive_follows, foreign_key: 'followed_id', class_name: 'Follow', dependent: :destroy
+  has_many :active_follows, foreign_key: 'follower_id', class_name: 'Follow', dependent: :destroy, inverse_of: 'follower'
+  has_many :passive_follows, foreign_key: 'followed_id', class_name: 'Follow', dependent: :destroy, inverse_of: 'followed'
   has_many :followings, through: :active_follows, source: :followed
   has_many :followers, through: :passive_follows, source: :follower
   has_one_attached :avatar
