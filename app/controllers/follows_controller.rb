dc945ad53a8f1b3305ddc07a262e599cd23f27cf
88-99 <xz835.525o39@yahoo.com>
2023-02-04 23:36:11 +0900

    Rubocop 修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,12 +2,12 @@
 
 class FollowsController < ApplicationController
   def create
-    following = current_user.follow(params[:format])
+    current_user.follow(params[:format])
     redirect_back fallback_location: users_path, notice: t('controllers.success.notice_follow')
   end
 
   def destroy
-    following = current_user.unfollow(params[:id])
+    current_user.unfollow(params[:id])
     redirect_back fallback_location: users_path, notice: t('controllers.success.notice_unfollow')
   end
 end
