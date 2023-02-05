b1786f8d83fbd5c493850006a9c9f88f0e1a09cf
88-99 <xz835.525o39@yahoo.com>
2023-02-05 23:54:19 +0900

    リダイレクトを修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,11 +3,11 @@
 class FollowsController < ApplicationController
   def create
     current_user.follow(params[:format])
-    redirect_back fallback_location: users_path, notice: t('controllers.success.notice_follow')
+    redirect_to user_path(params[:format]), notice: t('controllers.success.notice_follow')
   end
 
   def destroy
     current_user.unfollow(params[:id])
-    redirect_back fallback_location: users_path, notice: t('controllers.success.notice_unfollow')
+    redirect_to user_path(params[:id]), notice: t('controllers.success.notice_unfollow')
   end
 end
