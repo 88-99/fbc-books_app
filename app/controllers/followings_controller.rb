6e93a7c4e5b000b99f248f5a8e1328c0d2f230a1
88-99 <xz835.525o39@yahoo.com>
2023-02-08 23:00:29 +0900

    create, destroyメソッドを修正

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,14 +2,13 @@
 
 class FollowingsController < ApplicationController
   def create
-    following = current_user.active_follows.build(followed_id: params[:user_id])
-    following.save
+    current_user.active_follows.create!(followed_id: params[:user_id])
     redirect_to user_path(params[:user_id]), notice: t('controllers.success.notice_follow')
   end
 
   def destroy
     following = current_user.active_follows.find_by!(followed_id: params[:user_id])
-    following.delete
+    following.destroy
     redirect_to user_path(params[:user_id]), notice: t('controllers.success.notice_unfollow')
   end
 end
