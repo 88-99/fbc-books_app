3850748ac8ec944e7b7c65a064ee0dc9a7e12bef
88-99 <xz835.525o39@yahoo.com>
2023-02-12 19:55:33 +0900

    ポリモーフィック関連を使用して、本と日報にコメントが付けられるよう実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :comment_params
+  before_action :set_commentable
+
   def create
-    @comment = current_user.comments.new(comment_params)
+    @comment = @commentable.comments.new(comment_params)
     @comment.save!
     redirect_back(fallback_location: root_path)
   end
 
   private
 
+  def set_commentable
+    type, id = request.path.split('/')[1, 2]
+    @commentable =
+      case type
+      when 'reports'
+        Report.find_by! id: id
+      when 'books'
+        Book.find_by! id: id
+      end
+  end
+
   def comment_params
-    params.require(:comment).permit(:content).merge(report_id: params[:report_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
