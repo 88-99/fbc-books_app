e97ec98751b093fe8dc70b5a73ee9a80e807ebf2
88-99 <xz835.525o39@yahoo.com>
2023-03-10 12:35:49 +0900

    ReportModelのテストを実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?(target_user)' do
+    alice = User.find_by(name: 'Alice')
+    bob = User.find_by(name: 'Bob')
+    report = Report.new(user_id: alice.id, title: '日報のタイトル', content: '日報の内容')
+
+    assert report.editable?(alice)
+    assert_not report.editable?(bob)
+  end
+
+  test '#created_on' do
+    report = Report.new(created_at: Time.current, title: '日報のタイトル', content: '日報の内容')
+    assert_equal Time.current.to_date, report.created_on
+  end
 end
