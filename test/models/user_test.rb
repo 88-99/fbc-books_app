b4b9107e90fdaa54b95682dc348e38809cfc4607
88-99 <xz835.525o39@yahoo.com>
2023-03-18 00:24:57 +0900

    UserModelのテストを修正&追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @alice = users(:Alice)
+    @bob = users(:Bob)
+  end
+
   test '#name_or_email' do
     user = User.new(email: 'foo@example.com', name: '')
     assert_equal 'foo@example.com', user.name_or_email
@@ -12,20 +17,19 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'フォローする' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+    assert_equal @bob.active_relationships.find_or_create_by!(following_id: @alice.id), @bob.follow(@alice)
   end
 
   test 'フォローを解除する' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
+    relationship = @bob.active_relationships.find_by(following_id: @alice)
+    assert_equal relationship&.destroy!, @bob.unfollow(@alice)
+  end
 
-    me.follow(she)
-    me.unfollow(she)
-    assert_not me.following?(she)
+  test 'ユーザをフォローしているかどうか' do
+    assert_equal @bob.active_relationships.where(following_id: @alice.id).exists?, @bob.following?(@alice)
+  end
+
+  test 'ユーザにフォローされているかどうか' do
+    assert_equal @bob.passive_relationships.where(follower_id: @alice.id).exists?, @bob.followed_by?(@alice)
   end
 end
